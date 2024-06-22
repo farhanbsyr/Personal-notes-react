363363220d9aba9b45e14f9e8826a399c3fe8d9e
unknown <farhanbsyr@gmail.com>
2024-06-23 03:00:55 +0700

    menghapus file apaun

--- src/components/NoteItemContent.jsx
+++ src/components/NoteItemContent.jsx
@@ -4,7 +4,7 @@ import NotesItemBody from "./NotesItemBody";
 
 const NotesItemContent = ({ createdAt, title, body }) => {
   return (
-    <div className="note-item__content  ">
+    <div className="note-item__content">
       <NotesItemTitle createdAt={createdAt} title={title} />
       <NotesItemBody body={body} />
     </div>
